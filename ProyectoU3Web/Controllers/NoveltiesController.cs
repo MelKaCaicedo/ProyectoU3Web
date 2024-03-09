4923e09739549985fe0a9c5b1d1bfe6df7740948
MelKaCaicedo <mkcaicedo@espe.edu.ec>
2024-03-09 13:59:34 -0500

    prueba1

---
+++ ProyectoU3Web/Controllers/NoveltiesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoU3Web.Controllers
+{
+    public class NoveltiesController : Controller
+    {
+        // GET: Novelties
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
